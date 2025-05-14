42f054fbb1068536e50c490f15d5735b7deb851f
Andrew Lisov <lisov@MacBook-Pro-Andrew.local>
2025-05-14 15:19:44 +0200

    update dependencies

--- src/main/java/ppp/plugin/keycloak/login/OtpPasswordAuthenticatorFactory.java
+++ src/main/java/ppp/plugin/keycloak/login/OtpPasswordAuthenticatorFactory.java
@@ -15,73 +15,73 @@ import org.keycloak.provider.ProviderConfigProperty;
 
 public class OtpPasswordAuthenticatorFactory implements AuthenticatorFactory {
 
-    public static final String ID = "otppasswordauthenticator";
+  public static final String ID = "otppasswordauthenticator";
 
-    private static final Authenticator AUTHENTICATOR_INSTANCE = new OtpPasswordAuthenticator();
-    static final String OTP_VALIDATION_EXTERNAL_SERVICE_URL = "otp-validation-service-url";
+  private static final Authenticator AUTHENTICATOR_INSTANCE = new OtpPasswordAuthenticator();
+  static final String OTP_VALIDATION_EXTERNAL_SERVICE_URL = "otp-validation-service-url";
 
-    @Override
-    public Authenticator create(KeycloakSession keycloakSession) {
-        return AUTHENTICATOR_INSTANCE;
-    }
+  @Override
+  public Authenticator create(KeycloakSession keycloakSession) {
+    return AUTHENTICATOR_INSTANCE;
+  }
 
-    @Override
-    public String getDisplayType() {
-        return "Otp Password Authenticator";
-    }
+  @Override
+  public String getDisplayType() {
+    return "Otp Password Authenticator";
+  }
 
-    @Override
-    public boolean isConfigurable() {
-        return true;
-    }
+  @Override
+  public boolean isConfigurable() {
+    return true;
+  }
 
-    @Override
-    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
-        return new AuthenticationExecutionModel.Requirement[] { AuthenticationExecutionModel.Requirement.REQUIRED };
-    }
+  @Override
+  public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+    return new AuthenticationExecutionModel.Requirement[] { AuthenticationExecutionModel.Requirement.REQUIRED };
+  }
 
-    @Override
-    public boolean isUserSetupAllowed() {
-        return false;
-    }
+  @Override
+  public boolean isUserSetupAllowed() {
+    return false;
+  }
 
-    @Override
-    public String getHelpText() {
-        return "Limits access to only valid Otp or password";
-    }
+  @Override
+  public String getHelpText() {
+    return "Limits access to only valid Otp or password";
+  }
 
-    @Override
-    public List<ProviderConfigProperty> getConfigProperties() {
-        ProviderConfigProperty servicename = new ProviderConfigProperty();
-        servicename.setType(STRING_TYPE);
-        servicename.setName(OTP_VALIDATION_EXTERNAL_SERVICE_URL);
-        servicename.setLabel("OTP validation service url");
-        servicename.setDefaultValue("http://localhost:8080/api/validateOtp");
-        servicename.setHelpText("Valid url for OTP validation e.g. http://localhost:8080/api/validateOtp");
+  @Override
+  public List<ProviderConfigProperty> getConfigProperties() {
+    ProviderConfigProperty servicename = new ProviderConfigProperty();
+    servicename.setType(STRING_TYPE);
+    servicename.setName(OTP_VALIDATION_EXTERNAL_SERVICE_URL);
+    servicename.setLabel("OTP validation service url");
+    servicename.setDefaultValue("http://localhost:8080/api/validateOtp");
+    servicename.setHelpText("Valid url for OTP validation e.g. http://localhost:8080/api/validateOtp");
 
-        List<ProviderConfigProperty> listOfConfigs = Arrays.asList(servicename);
-        return listOfConfigs;
-    }
+    List<ProviderConfigProperty> listOfConfigs = Arrays.asList(servicename);
+    return listOfConfigs;
+  }
 
-    @Override
-    public String getReferenceCategory() {
-        return null;
-    }
+  @Override
+  public String getReferenceCategory() {
+    return null;
+  }
 
-    @Override
-    public void init(Config.Scope scope) {
-    }
+  @Override
+  public void init(Config.Scope scope) {
+  }
 
-    @Override
-    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
-    }
+  @Override
+  public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+  }
 
-    @Override
-    public void close() {
-    }
+  @Override
+  public void close() {
+  }
 
-    @Override
-    public String getId() {
-        return ID;
-    }
+  @Override
+  public String getId() {
+    return ID;
+  }
 }
